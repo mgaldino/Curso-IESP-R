a7bf4352435f5b42dd62cbabee71d301909d99aa
mgaldino <mcz.fea@gmail.com>
2014-07-20 00:50:35 -0300

    Coletando banco de texto (projeto gutemberg em pt) para Spelling Corrector.

---
+++ SpellingCorrector.R
@@ -0,0 +1,78 @@
+###################################
+#### Manoel Galdino 19/07.2014 ####
+### Spelling Corrector  ###########
+### Naive Bayes  ##################
+###################################
+
+
+## Baseado em: http://norvig.com/spell-correct.html
+
+wget -w 2 -m -H http://www.gutenberg.org/robot/harvest?filetypes[]=txt&langs[]=pt
+link <- url('http://www.gutenberg.org/robot/harvest?filetypes[]=html&langs[]=pt')
+
+## esperar 2 segundos
+
+
+url <- "http://www.gutenberg.org/robot/harvest?filetypes[]=html&langs[]=pt"
+doc <- htmlParse(url)
+links <- xpathSApply(doc, "//a/@href")
+free(doc)
+
+aux <- grep("offset", links)
+links[aux]
+# new url etc.
+
+## generalizando pra uma função
+## vamos usar append
+?append
+
+getMyLinks <- function ( primeiraPagina, StepOut = 3 ) {
+  url <- character()
+  url[1] <- primeiraPagina
+  condition <- T
+  i <- 1
+  links <- list()
+  while ( condition ) {
+    doc <- htmlParse(url[length(url)])
+    links[i] <- xpathSApply(doc, "//a/@href")
+    free(doc) 
+    aux <- grep("offset", links[i])
+    url <- append (url, aux)
+    i <- i + 1
+    if (!grepl("offset", links) ) {
+      condition <- F
+    }
+    if (i > StepOut) {
+      condition <- F
+    }
+    Sys.sleep(2)
+  }
+  return(links)
+}
+
+url <- "http://www.gutenberg.org/robot/harvest?filetypes[]=html&langs[]=pt"
+linksTeste <- getMyLinks(url )
+
+  
+  
+
+
+
+class(links)
+length(links)
+links[1]
+gsub("href", "", links[1])
+nchar("http://www.gutenberg.lib.md.us/1/3/0/9/13092/13092-h.zip")
+nchar(links[1])
+
+vec1 <- "a"
+microbenchmark(
+  vec1 <- append(vec1, "b"),
+  vec1[2] <- "b"
+) 
+
+
+Sys.sleep(2)
+
+
+
