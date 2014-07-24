175a7472a6186f98ec41891c98474d20be06a752
mgaldino <mcz.fea@gmail.com>
2014-07-24 16:38:18 -0300

    Limpei o script e deixei apenas o que estamos usando. Limpeza apenas com dtm.control.

--- AulaXTopicModels.R
+++ AulaXTopicModels.R
@@ -45,6 +45,9 @@ if(require("ggplot2") == F){install.packages('ggplot2'); library("ggplot2")}
 if(require("seqinr") == F) {install.packages("seqinr"); library("seqinr")}
 if(require("RTextTools") == F) {install.packages("RTextTools")
                                 library("RTextTools")}
+if(require(topicmodels) == F) {install.packages("topicmodels")
+                               library("topicmodels")}
+
 
 ######
 ## Análise de Dados de Revistas na Scielo
@@ -245,34 +248,12 @@ View(aux)
 
 # criando corpus ---------------------------------------------------------------------------------------------------------------
 
-### Vamos limpar o texto
-## Vamos usar o pacote tm (term matrix)
-## é o pacote mais usado para processar texto no R
-aux <- gsub("[[:punct:]*]", " ", aux) # para evitar casos em que o ponto não foi sucedido de espço
-aux <- trimSpace(aux) # para remover espaços do começo e do final da string
-# aux <- remove_acento(aux, Toupper=F) está em ingles
-aux <- tolower(aux)
-
-
-vec <- as.character(1939:2014)
-for ( i in 1:length(vec)) {
-  aux <- gsub(vec[i], "", aux)
-}
-
-
 extendedstopwords <- c("a","about","above","across","after",
                      "again","against","all","almost",
                      "alone","along","already","also",
                      "although","always","am","among",
                      "an","and","another","any","anybody","anyone","anything","anywhere","are","area","areas","aren't","around","as","ask","asked","asking","asks","at","away","b","back","backed","backing","backs","be","became","because","become","becomes","been","before","began","behind","being","beings","below","best","better","between","big","both","but","by","c","came","can","cannot","can't","case","cases","certain","certainly","clear","clearly","come","could","couldn't","d","did","didn't","differ","different","differently","do","does","doesn't","doing","done","don't","down","downed","downing","downs","during","e","each","early","either","end","ended","ending","ends","enough","even","evenly","ever","every","everybody","everyone","everything","everywhere","f","face","faces","fact","facts","far","felt","few","find","finds","first","for","four","from","full","fully","further","furthered","furthering","furthers","g","gave","general","generally","get","gets","give","given","gives","go","going","good","goods","got","great","greater","greatest","group","grouped","grouping","groups","h","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","her","here","here's","hers","herself","he's","high","higher","highest","him","himself","his","how","however","how's","i","i'd","if","i'll","i'm","important","in","interest","interested","interesting","interests","into","is","isn't","it","its","it's","itself","i've","j","just","k","keep","keeps","kind","knew","know","known","knows","l","large","largely","last","later","latest","least","less","let","lets","let's","like","likely","long","longer","longest","m","made","make","making","man","many","may","me","member","members","men","might","more","most","mostly","mr","mrs","much","must","mustn't","my","myself","n","necessary","need","needed","needing","needs","never","new","newer","newest","next","no","nobody","non","noone","nor","not","nothing","now","nowhere","number","numbers","o","of","off","often","old","older","oldest","on","once","one","only","open","opened","opening","opens","or","order","ordered","ordering","orders","other","others","ought","our","ours","ourselves","out","over","own","p","part","parted","parting","parts","per","perhaps","place","places","point","pointed","pointing","points","possible","present","presented","presenting","presents","problem","problems","put","puts","q","quite","r","rather","really","right","room","rooms","s","said","same","saw","say","says","second","seconds","see","seem","seemed","seeming","seems","sees","several","shall","shan't","she","she'd","she'll","she's","should","shouldn't","show","showed","showing","shows","side","sides","since","small","smaller","smallest","so","some","somebody","someone","something","somewhere","state","states","still","such","sure","t","take","taken","than","that","that's","the","their","theirs","them","themselves","then","there","therefore","there's","these","they","they'd","they'll","they're","they've","thing","things","think","thinks","this","those","though","thought","thoughts","three","through","thus","to","today","together","too","took","toward","turn","turned","turning","turns","two","u","under","until","up","upon","us","use","used","uses","v","very","w","want","wanted","wanting","wants","was","wasn't","way","ways","we","we'd","well","we'll","wells","went","were","we're","weren't","we've","what","what's","when","when's","where","where's","whether","which","while","who","whole","whom","who's","whose","why","why's","will","with","within","without","won't","work","worked","working","works","would","wouldn't","x","y","year","years","yes","yet","you","you'd","you'll","young","younger","youngest","your","you're","yours","yourself","yourselves","you've","z")
 regexextendedstopwords <- paste("\\b", extendedstopwords, "\\b", sep="")
-
-removeStopoWords <- function (vetor, lista.words) {
-  #browser()
-  egexextendedstopwords <- paste("\\b", lista.words, "\\b", sep="")
-  for ( i in 1:length(extendedstopwords)) vetor <- gsub(regexextendedstopwords[i], "", vetor)
-   return(vetor) 
-}
 
 aux1 <- removeStopoWords(aux, extendedstopwords)  
 
@@ -290,28 +271,15 @@ dtm.control <- list(
 
 
 # transformando vetor de documentos em corpus
-abstractBPSR <- Corpus(VectorSource(aux1)) ## control nao funfou ??
+abstractBPSR <- Corpus(VectorSource(aux)) 
 
-## removendo stopwords etc.
-## outra forma
-
-abstractBPSR <- tm_map(abstractBPSR, stemDocument)
-abstractBPSR <- tm_map(abstractBPSR, content_transformer(tolower))
-abstractBPSR <- tm_map(abstractBPSR, removeWords, stopwords("english")) 
-abstractBPSR <- tm_map(abstractBPSR, content_transformer(removeNumbers))
-abstractBPSR <- tm_map(abstractBPSR, content_transformer(removePunctuation))
-
-
-# criando uma DocumentTermMatrix 
-# é usada como argumento de LDA
-# dtm.control é pra ser usada aqui
-dtm <- DocumentTermMatrix(abstractBPSR, control=dtm.list)
+dtm <- DocumentTermMatrix(abstractBPSR, control=dtm.control)
 dim(dtm)
 ## cada doc é uma linha
 # cada palavra uma coluna
 # número de colunas e igual número de palavras únicas em todos os docs
 View(dtm) ## erro. Tem que usar inspect
-inspect(dtm[1:10,1:20])
+inspect(dtm[1:10,850:872])
 
 
 # dtm1 <- removeSparseTerms(dtm,0.95)
@@ -322,8 +290,6 @@ inspect(dtm[1:10,1:20])
 #dtm <- dtm[rowSums(as.matrix(dtm))>0,]
 
 #### Topic Modeling - LDA ####
-set.seed(51)
-trainpoints <- sample(1:nrow(dtm),1.0*nrow(dtm),replace=F) # to train on a subsample, change 1.0 to a lower value, say 0.8
 
 ### Qtos tópicos? 3?
 ## Número pequeno, pois tenho poucos docs
